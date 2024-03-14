5f810d32519e554951c58ffe5b757e10c1fb3860
Bikram Sadhukhan <117967710+biku34@users.noreply.github.com>
2024-03-14 10:41:01 +0530

    Delete MyMusic directory

--- MyMusic/song.js
+++
@@ -1,7 +0,0 @@
-function playSong(songPath) {
-            var audio = document.getElementById("audioPlayer");
-            var source = document.getElementById("audioSource");
-            source.src = songPath;
-            audio.load();
-            audio.play();
-        }
